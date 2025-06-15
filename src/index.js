bac1b33b8b3a6d5a46b87562e2651b5eb23991a4
Bluchip Studio <103538584+BluchipStudio@users.noreply.github.com>
2025-06-15 19:54:31 +0100

    feat: propagate decryption errors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const nativeModule = require('bindings')('mp4decrypt-buffer')
  * @returns {Promise<Buffer>}
  */
 exports.decrypt = (buffer, keyMap) => {
-  return new Promise(resolve => {
-    nativeModule.decrypt(buffer, keyMap, resolve)
+  return new Promise((resolve, reject) => {
+    nativeModule.decrypt(buffer, keyMap, (err, result) => {
+      if (err) return reject(err)
+      resolve(result)
+    })
   })
 }
